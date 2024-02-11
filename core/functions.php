3d4fb4c5f535ed50ec2dfacb78832ebfaec32a33
niltonpaes <niltonpa00@gmail.com>
2024-02-11 13:12:40 -0500

    sql dump and also deletes some files that were not beeing used

--- core/functions.php
+++ core/functions.php
@@ -1,7 +1,5 @@
 <?php
 
-use Core\Response;
-
 function dd($value)
 {
 	echo "<pre>";
@@ -9,29 +7,6 @@ function dd($value)
 	echo "</pre>";
 
 	// die();
-}
-
-function urlIs($value)
-{
-	return $_SERVER["REQUEST_URI"] === $value;
-}
-
-function abort($code = 404)
-{
-	http_response_code($code);
-
-	require base_path("views/{$code}.php");
-
-	die();
-}
-
-function authorize($condition, $status = Response::FORBIDDEN)
-{
-	if (!$condition) {
-		abort($status);
-	}
-
-	return true;
 }
 
 function base_path($path)
@@ -44,22 +19,4 @@ function view($path, $attributes = [])
 	extract($attributes);
 
 	require base_path("views/" . $path);
-}
-
-function login($user)
-{
-	$_SESSION["user"] = [
-		"email" => $user["email"],
-	];
-
-	session_regenerate_id(true);
-}
-
-function logout()
-{
-	$_SESSION = [];
-	session_destroy();
-
-	$params = session_get_cookie_params();
-	setcookie("PHPSESSID", "", time() - 3600, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
 }
